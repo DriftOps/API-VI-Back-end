5c9bf504606a708795b32af30a78ef752d67b23d
Felipe Pereira <126419915+felipereira10@users.noreply.github.com>
2025-08-21 18:14:47 -0300

    feat. st commit

---
+++ src/main/java/com/xertica/model/User.java
@@ -0,0 +1,20 @@
+package com.xertica.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String goal;
+}
